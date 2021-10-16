27acd7a1c74614a587185a59c5a615df75c82229
bwanglzu <bo.wang@jina.ai>
2021-10-16 18:18:18 +0200

    refactor: align traversal path naming

--- match_merger.py
+++ match_merger.py
@@ -11,18 +11,16 @@ class MatchMerger(Executor):
     The MatchMerger merges the results of shards by appending all matches..
     """
 
-    def __init__(self, default_traversal_paths: Tuple[str, ...] = ('r',), **kwargs):
+    def __init__(self, traversal_paths: Tuple[str, ...] = ('r',), **kwargs):
         """
-        :param default_traversal_paths: traverse path on docs, e.g. ['r'], ['c']
+        :param traversal_paths: traverse path on docs, e.g. ['r'], ['c']
         """
         super().__init__(**kwargs)
-        self.default_traversal_paths = default_traversal_paths
+        self.traversal_paths = traversal_paths
 
     @requests
     def merge(self, docs_matrix: List[DocumentArray], parameters: dict, **kwargs):
-        traversal_paths = parameters.get(
-            'traversal_paths', self.default_traversal_paths
-        )
+        traversal_paths = parameters.get('traversal_paths', self.traversal_paths)
         results = {}
         for docs in docs_matrix:
             self._merge_shard(results, docs, traversal_paths)
